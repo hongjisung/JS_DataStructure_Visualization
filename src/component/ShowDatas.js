41c2c80a68563e95ad0f65159b2d9bd5d142e84e
jisung <rhfktj@gmail.com>
2019-02-01 12:31:14 +0900

    ShowDatas Component
    show the variables in operating

---
+++ src/component/ShowDatas.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import '../stylesheet/ShowDatas.css'
+
+const ShowDatas = ({dataStates = []}) => {
+  const width = 63
+  return (
+    <div className='show-datas'>
+      <svg className='data-svg'>
+        {dataStates.map((data,i) => {
+          return (
+            <rect key={i} x={10*(i+1) + width*i} y="10" width={width} height="20" style={{fill:"aliceblue", stroke:"black"}} />
+          )
+        })}
+        {dataStates.map((data,i) => {
+          return (
+            <text key={i} x={12*(i+1) + width*i} y="25" width={width} height="20" filled="red">{data.name.substring(0, 6)}</text>
+          )
+        })}
+        {dataStates.map((data,i) => {
+          return (
+            <rect key={i} x={10*(i+1) + width*i} y="30" width={width} height="30" style={{fill:"lightgray", stroke: "black"}} />
+          )
+        })}
+        {dataStates.map((data,i) => {
+          return (
+            <text key={i} x={12*(i+1) + width*i} y="50" width={width} height="20">{(typeof data.value === 'object'?data.value.constructor.name.substring(0, 6): (typeof data.value).substring(0, 6))}</text>
+          )
+        })}
+      </svg>
+    </div>
+  )
+}
+
+ShowDatas.propTypes = {
+  dataStates: PropTypes.array
+}
+
+export default ShowDatas
