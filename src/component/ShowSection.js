22f84510b4540fa45735c8b233e35c9562d7c753
jisung <rhfktj@gmail.com>
2019-02-01 12:30:20 +0900

    ShowSection Component
    visualize the data and container

---
+++ src/component/ShowSection.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import '../stylesheet/ShowSection.css'
+import ShowDatas from './ShowDatas'
+import ShowContainer from './ShowContainer'
+
+const ShowSection = ({ dataStates=[], executingCode="", containerState={} }) =>
+  <section className='show-section'>
+  <div className='text-show1'>데이터 상태</div>
+    <ShowDatas dataStates={dataStates} />
+    <div className='text-show2'>컨테이너 상태</div>
+    <div className='text-show3'>실행코드: {executingCode}</div>
+    <ShowContainer containerState={containerState} />
+  </section>
+
+ShowSection.propTypes = {
+  dataStates: PropTypes.array,
+  executingCode: PropTypes.string,
+  containerState: PropTypes.object
+}
+
+export default ShowSection
