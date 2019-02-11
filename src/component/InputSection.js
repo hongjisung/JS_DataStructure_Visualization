af5d7fd7a7f611e5cba59b3000c02df243a5d36c
jisung <rhfktj@gmail.com>
2019-02-11 16:52:49 +0900

    add Instructions for writing code.

--- src/component/InputSection.js
+++ src/component/InputSection.js
@@ -7,25 +7,46 @@ import '../stylesheet/InputSection.css'
 class InputSection extends Component {
   constructor() {
     super()
-    this.state = {submit:false};
+    this.state = {
+      submit:false,
+      showCaution: false,
+    };
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    const {submit} = this.state;
-    return !submit && nextState.submit
+    const {submit, showCaution} = this.state;
+    return (!submit && nextState.submit) || (showCaution !== nextState.showCaution)
   }
   componentDidUpdate() {
     this.setState({submit: false})
+  }
+
+  changeShow = () => {
+    console.log('click')
+    this.setState({showCaution: !this.state.showCaution})
   }
 
   render() {
     return (
       <section className='input-section'>
         <div className='text-input1'>코드 작성</div>
+        <button className='precaution' onClick={this.changeShow}><img className='cautionImg' src={require('../public/caution.png')} alt='caution'/></button>    
         <button className='input-button' onClick={input => this.setState({submit: true})}>제출</button>
         <InputCode submit={this.state.submit} getCode={this.props.getCode}/>
         <div className='text-input2'>인풋 데이타 json</div>
         <InputData submit={this.state.submit} getData={this.props.getData}/>
+        {(this.state.showCaution)?
+        <div className='coverDom'>
+        <div className='cautionContent'>
+          <p>1. Declare variables at the top of the scope block.</p>
+          <p>2. Method Chaining does not animationized</p>
+          <p>3. The method in parameter does not animationized</p>
+          <p>4. Semicolon is need to the end of line</p>
+          <p>5. paste the '.' and name</p>
+          <button className='input-button' onClick={this.changeShow}>close</button>
+        </div>
+        </div>
+        :null}
       </section>
     )
   }
