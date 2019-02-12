92ccd8ff45f96e112b57627505f6a4f58e02ecad
jisung <rhfktj@gmail.com>
2019-02-12 17:08:11 +0900

    Updates

---
+++ precache-manifest.96d400be4f32bd5f26a3dbf4bf5cc9af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c4bd81cde1ab53ac73e",
+    "url": "/JS_DataStructure_Visualization/static/js/runtime~main.0c4bd81c.js"
+  },
+  {
+    "revision": "af50602af556ead37c95",
+    "url": "/JS_DataStructure_Visualization/static/js/main.af50602a.chunk.js"
+  },
+  {
+    "revision": "460d1066090ea51d7ce0",
+    "url": "/JS_DataStructure_Visualization/static/js/1.460d1066.chunk.js"
+  },
+  {
+    "revision": "af50602af556ead37c95",
+    "url": "/JS_DataStructure_Visualization/static/css/main.b36b7dab.chunk.css"
+  },
+  {
+    "revision": "af43e6955c77e3fe8c43970fa81e6a18",
+    "url": "/JS_DataStructure_Visualization/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/js_datastructure_visualization/precache-manifest.16141b935e3dfc2d008ee8515a0d27ba.js"
+  "/JS_DataStructure_Visualization/precache-manifest.96d400be4f32bd5f26a3dbf4bf5cc9af.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/js_datastructure_visualization/index.html", {
+workbox.routing.registerNavigationRoute("/JS_DataStructure_Visualization/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
