696507b0b59c82ff2fd2294e95c8367d1848661a
jisung <rhfktj@gmail.com>
2019-02-14 15:26:38 +0900

    add animation paramenters

--- src/component/container/Arrow.js
+++ src/component/container/Arrow.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const Arrow = (startx, starty, endx, endy, className="Arrow") => {
+const Arrow = (startx, starty, endx, endy, className="Arrow", key=0, ani_delay='0s', ani_dur='0s') => {
   const midx = startx/6 + endx*5/6
   const midy = starty/6 + endy*5/6
   const diffx = (endy-starty)/12
   const diffy = (endx-startx)/12
   return (
     [
-      <line className={className} x1={startx} y1={starty} x2={endx} y2={endy} style={{stroke: "black", strokeWidth:1}}/>,
-      <line className={className} x1={midx+diffx} y1={midy-diffy} x2={endx} y2={endy} style={{stroke: "black", strokeWidth:1}}/>,
-      <line className={className} x1={midx-diffx} y1={midy+diffy} x2={endx} y2={endy} style={{stroke: "black", strokeWidth:1}}/>
+      <line key={key} className={className} x1={startx} y1={starty} x2={endx} y2={endy} style={{stroke: "black", strokeWidth:1, animationDelay: ani_delay, animationDuration: ani_dur}}/>,
+      <line key={-1*key} className={className} x1={midx+diffx} y1={midy-diffy} x2={endx} y2={endy} style={{stroke: "black", strokeWidth:1, animationDelay: ani_delay, animationDuration: ani_dur}}/>,
+      <line key={1000000*key} className={className} x1={midx-diffx} y1={midy+diffy} x2={endx} y2={endy} style={{stroke: "black", strokeWidth:1, animationDelay: ani_delay, animationDuration: ani_dur}}/>
     ]
   )
 }
