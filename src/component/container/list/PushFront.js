369af96d299fd16ff626fc683c9401378d8f5510
jisung <rhfktj@gmail.com>
2019-02-14 15:46:34 +0900

    correct animation cod eof pushFront

--- src/component/container/list/PushFront.js
+++ src/component/container/list/PushFront.js
@@ -4,7 +4,7 @@ import Arrow from '../Arrow'
 import '../../../stylesheet/container/list/PushFront.css'
 
 
-const PushFront = ({initiate=f=>f, object, params=[]}) => {
+const PushFront = ({initiate=f=>f, object, params=[], duration = 1}) => {
   const list = object
   let itr = list.begin();
   const express = []
@@ -12,26 +12,31 @@ const PushFront = ({initiate=f=>f, object, params=[]}) => {
   const width = 65;
   const interval = 20;
   const shownodenum=7;
+  let keyid = 1;
 
   // execute next code
-  initiate(2000)
+  initiate(duration*2*1000)
 
   let count = (list.size()>shownodenum)?shownodenum : list.size();
   if(count) {
-    express.push(<text x={interval} y={40} width={30} height={15}>Front</text>)
+    express.push(<text key={keyid} x={interval} y={40} width={30} height={15}>Front</text>)
+    keyid += 1;
   } else {
-    express.push(<text className='pushFrontNew' x={interval} y={40} width={30} height={15}>Front</text>)
+    express.push(<text key={keyid} className='appear' style={{animationDelay:duration.toString()+'s', animationDuration:duration.toString()+'s'}} x={interval} y={40} width={30} height={15}>Front</text>)
+    keyid += 1;
   }
 
-  express.push(<text className='stackPushSizeDown' x={interval} y={20} width={30} height={15}>size: {list.size()}</text>)
-  express.push(<text className='stackPushSizeUp' x={interval} y={20} width={30} height={15}>size: {list.size() + 1}</text>)
-
+  express.push(<text key={keyid} className='pqPopSizeErase' style={{animationDuration: (duration*2).toString() +'s'}} x={interval} y={20} width={30} height={15}>size: {list.size()}</text>)
+  keyid += 1;
+  express.push(<text key={keyid} className='pqPopSizeEmerge' style={{animationDuration: (duration*2).toString() +'s'}}  x={interval} y={20} width={30} height={15}>size: {list.size() + 1}</text>)
+  keyid += 1;
   
   // draw node
   let dataitr = 1;
   while (itr !== list.end() && count>=dataitr) {
     const data = itr.getData();
-    DataNode({"className": "front_origin", "key": dataitr, "data": data.toString(), x : interval*(dataitr) + width*(dataitr-1), y: 50, "width": width}).map(n => express.push(n))
+    DataNode({key: keyid, ani_delay:'0s', ani_dur: duration.toString()+'s', "className": "front_origin", "data": data.toString(), x : interval*(dataitr) + width*(dataitr-1), y: 50, "width": width}).map(n => express.push(n))
+    keyid += 1;
     dataitr += 1
     itr = itr.getNext();
   }
@@ -39,16 +44,21 @@ const PushFront = ({initiate=f=>f, object, params=[]}) => {
   // draw arrow
   count = (list.size()>shownodenum)?shownodenum : list.size();
   for (let i=1; i<count; i += 1) {
-    Arrow(interval * i + width * i, 70, interval * (i+1) + width * i, 70, "front_origin").map(n => express.push(n))
-    Arrow(interval * (i+1) + width * i, 80, interval * i + width * i, 80, "front_origin").map(n => express.push(n))
+    Arrow(interval * i + width * i, 70, interval * (i+1) + width * i, 70, "front_origin", keyid, '0s', duration.toString()+'s').map(n => express.push(n))
+    keyid += 1;
+    Arrow(interval * (i+1) + width * i, 80, interval * i + width * i, 80, "front_origin", keyid, '0s', duration.toString()+'s').map(n => express.push(n))
+    keyid += 1;
   }
 
   // New Node and Arrow
   count = (list.size()>0)?1 : 0;
-  DataNode({"className": "pushFrontNew", "key": count+1,"data": params[0].toString(),  x : interval, y: 50, "width": width}).map(n => newNodeArrow.push(n))
+  DataNode({key: keyid, ani_delay: duration.toString()+'s', ani_dur: duration.toString()+'s',"className": "appear","data": params[0].toString(),  x : interval, y: 50, "width": width}).map(n => newNodeArrow.push(n))
+  keyid += 1;
   if (count) {
-    Arrow(interval * count + width * count, 70, interval * (count+1) + width * count, 70, "pushFrontNew").map(n => newNodeArrow.push(n))
-    Arrow(interval * (count+1) + width * count, 80, interval * count + width * count, 80, "pushFrontNew").map(n => newNodeArrow.push(n))
+    Arrow(interval * count + width * count, 70, interval * (count+1) + width * count, 70, "appear", keyid, duration.toString()+'s', duration.toString()+'s').map(n => newNodeArrow.push(n))
+    keyid += 1;
+    Arrow(interval * (count+1) + width * count, 80, interval * count + width * count, 80, "appear", keyid, duration.toString()+'s', duration.toString()+'s').map(n => newNodeArrow.push(n))
+    keyid += 1;
   }
 
   return (
