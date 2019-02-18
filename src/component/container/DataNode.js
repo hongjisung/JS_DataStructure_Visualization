dd47b96fb65c9ecd3a544c07fe0a5a931f018438
jisung <rhfktj@gmail.com>
2019-02-18 13:03:26 +0900

    add title to rect

--- src/component/container/DataNode.js
+++ src/component/container/DataNode.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+// import '../../stylesheet/DataNode.css'
 
 const DataNode = ({data='', x=10, y=20, width=60, height=50, color='lightcyan', key = 1, className="DataNode", border="black", ani_dur="2s", ani_delay='1s'}) => {
   return (
-    [<rect className={className} key={key} x={x} y={y} width={width} height={height} style={{animationDuration: ani_dur, animationDelay: ani_delay, "fill": color, "stroke":border, "strokeWidth":'1.2px'}}></rect>,
+    [<rect onClick={()=>{console.log(data.substring())}} className={className} key={key} x={x} y={y} width={width} height={height} style={{cursor: 'pointer', animationDuration: ani_dur, animationDelay: ani_delay, "fill": color, "stroke":border, "strokeWidth":'1.2px'}}>
+      <title>{data}</title>
+    </rect>,
     <text className={className} key={-1*key} x={x+2} y={y+height/2+5} width={width} height={20} style={{animationDuration: ani_dur, animationDelay: ani_delay}}>{data.substring(0,6)}</text>]
   )
 }
