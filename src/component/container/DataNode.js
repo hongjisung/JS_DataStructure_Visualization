68813f507127409379b28b624c25a4e11e2345e7
jisung <rhfktj@gmail.com>
2019-02-19 14:18:21 +0900

    change datanode

--- src/component/container/DataNode.js
+++ src/component/container/DataNode.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 
 const DataNode = ({data='', x=10, y=20, width=60, height=50, color='lightcyan', key = 1, className="DataNode", border="black", ani_dur="2s", ani_delay='1s'}) => {
   return (
-    [<rect onClick={()=>{console.log(data.substring())}} className={className} key={key} x={x} y={y} width={width} height={height} style={{cursor: 'pointer', animationDuration: ani_dur, animationDelay: ani_delay, "fill": color, "stroke":border, "strokeWidth":'1.2px'}}>
+    [<rect className={className} key={key} x={x} y={y} width={width} height={height} style={{animationDuration: ani_dur, animationDelay: ani_delay, "fill": color, "stroke":border, "strokeWidth":'1.2px'}}>
       <title>{data}</title>
     </rect>,
     <text className={className} key={-1*key} x={x+2} y={y+height/2+5} width={width} height={20} style={{animationDuration: ani_dur, animationDelay: ani_delay}}>
